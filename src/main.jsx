4851e03b2f9c858d9a9c1e681c113d98a2c2d79e
kiky dolphin <rizfirsy@yahoo.com>
2023-01-21 23:41:16 +0700

    fix use effect problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Home from "./pages/Home";
 import "./index.css";
 import ThemeProvider from "./store/ThemeProvider";
 import ErrorPage from "./ErrorPage";
@@ -10,11 +10,11 @@ import Country from "./Country";
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
     errorElement: <ErrorPage />,
   },
   {
-    path: "/country:id",
+    path: "/:id",
     element: <Country />,
   },
 ]);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,182 @@
+import "./Home.css";
+import { useEffect, useState, useContext } from "react";
+import { Theme } from "../store/ThemeContext";
+
+function getCountriesByRegion(region) {
+  fetch(`https://restcountries.com/v3.1/region/${region}`).then((response) => {
+    if (!response.ok) throw alert("Countries not found with that filter!");
+    return response.json();
+  });
+}
+
+const Card = ({ country }) => {
+  const { name, flags, population, region, capital } = country;
+
+  const { darkMode } = useContext(Theme);
+
+  function detailHandler(ev) {
+    console.log("target :>> ", ev.target.parentElement);
+  }
+
+  return (
+    <div className={`card card-${darkMode ? "dark" : "light"}`}>
+      <button className="button btn-details">
+        <a href="#" onClick={detailHandler}>
+          Details
+        </a>
+      </button>
+      <div className="flag">
+        <img src={flags.svg} alt={`${name.official} + flag`} />
+      </div>
+      <div className="details">
+        <h4 className="country-name">{name.official}</h4>
+        <p className="country-detail">
+          Population: <span className="detail">{population}</span>
+        </p>
+        <p className="country-detail">
+          Region: <span className="detail">{region}</span>
+        </p>
+        <p className="country-detail">
+          Capital:{" "}
+          {capital === undefined ? (
+            <span className="detail">-</span>
+          ) : (
+            capital.map((city, index) => (
+              <span key={index} className="detail">
+                {city}
+              </span>
+            ))
+          )}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Header = () => {
+  const { darkMode, toggleDarkMode } = useContext(Theme);
+  function toggleTheme() {
+    toggleDarkMode();
+  }
+  return (
+    <header className={`header header-${darkMode ? "dark" : "light"}`}>
+      <h2>Where in the world?</h2>
+      <button
+        className={`button button-${darkMode ? "dark" : "light"}`}
+        onClick={toggleTheme}
+      >
+        {darkMode ? "🌒dark" : "🌖light"}
+      </button>
+    </header>
+  );
+};
+
+const Browser = ({ searchCountries }) => {
+  const [input, setInput] = useState("");
+  const [selectedRegion, setSelectedRegion] = useState("-");
+
+  const { darkMode } = useContext(Theme);
+
+  function searchHandler(ev) {
+    ev.preventDefault();
+    searchCountries(input, selectedRegion);
+  }
+
+  function onChangeRegion(ev) {
+    setSelectedRegion(ev.target.value);
+  }
+
+  function changeInputHandler(ev) {
+    setInput(ev.target.value);
+  }
+
+  return (
+    <section className="browser">
+      <form className="search-form">
+        <input
+          className={`search search-${darkMode ? "dark" : "light"}`}
+          name="search"
+          type="text"
+          placeholder="Try Indonesia..."
+          value={input}
+          onChange={changeInputHandler}
+        />
+        <select
+          className={`region region-${darkMode ? "dark" : "light"}`}
+          value={selectedRegion}
+          onChange={onChangeRegion}
+        >
+          <option value="-">All Region</option>
+          <option value="africa">Africa</option>
+          <option value="asia">Asia</option>
+          <option value="america">America</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
+        </select>
+        <button
+          className={`button button-${darkMode ? "dark" : "light"}`}
+          type="submit"
+          onClick={searchHandler}
+        >
+          Search
+        </button>
+      </form>
+    </section>
+  );
+};
+
+function Home() {
+  const [countries, setCountries] = useState([]);
+  const [input, setInput] = useState("");
+  const [region, setRegion] = useState("-");
+
+  console.log("typeof countries :>> ", typeof countries);
+
+  const { darkMode } = useContext(Theme);
+
+  function searchCountriesHandler(input, region) {
+    setInput(input);
+    setRegion(region);
+  }
+
+  useEffect(() => {
+    if (region === "-" && input === "") {
+      fetch("https://restcountries.com/v3.1/all")
+        .then((response) => response.json())
+        .then((data) => setCountries((prev) => (prev = data)));
+    } else if (region) {
+      fetch(`https://restcountries.com/v3.1/region/${region}`)
+        .then((response) => response.json())
+        .then((data) => setCountries((prev) => (prev = data)));
+    }
+  }, [input, region]);
+
+  return (
+    <div>
+      <Header />
+      <article className={`container container-${darkMode ? "dark" : "light"}`}>
+        <Browser searchCountries={searchCountriesHandler} />
+        <p>
+          Showing you{" "}
+          <span className="total-countries">{countries.length}</span> countries
+          around the world.
+        </p>
+        <div className="countries">
+          {countries
+            ? countries.map((country, index) => (
+                <Card key={index} country={country} />
+              ))
+            : "loading..."}
+        </div>
+      </article>
+      <footer className="footer">
+        <p>
+          Make with ☕ by{" "}
+          <a href="https://instagram.com/rizfirsy.dev">@rizfirsy.dev</a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
