aeaf8e50c9885b5f764e29748d63ed7de57bf003
kiky dolphin <rizfirsy@yahoo.com>
2023-01-21 23:01:31 +0700

    add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import ThemeProvider from "./store/ThemeProvider";
+import ErrorPage from "./ErrorPage";
+import Country from "./Country";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/country:id",
+    element: <Country />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={routes} />
     </ThemeProvider>
   </React.StrictMode>
 );
